551d5c818898c012b2157b33ab06aa6294481ba7
Na Lin <lna483018@gmail.com>
2022-12-31 21:08:57 -0500

    add error handle route for 404 and error handler

--- app.js
+++ app.js
@@ -10,4 +10,28 @@ app.get("/test", (req, res) => {
   res.send("APP for Learning Auth");
 });
 
+// @desc Create new user
+// @route: POST /api/users/signup
+
+// @desc 404 Not Found error message
+// @route -
+// @access -
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+// @desc Error handler
+// @route -
+// @access -
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+});
+
 module.exports = app;
