536f3fbed1d659c291354a93e0561a8cceefc198
Na Lin <lna483018@gmail.com>
2023-01-01 13:38:38 -0500

    add more guard to update password

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,22 +4,34 @@ const { User } = require("../db");
 const updatePassword = asyncHandler(async (req, res, next) => {
   const user = await User.findByPk(req.params.id);
 
-  const { password, passwordConfirm } = req.body;
-  if (!password || !passwordConfirm) {
+  // make sure oldpassword, new password, password confirm exist
+  const { oldPassword, newPassword, passwordConfirm } = req.body;
+  if (!oldPassword || !newPassword || !passwordConfirm) {
     res.status(400);
-    throw new Error("Please provide password and password confirm.");
+    throw new Error(
+      "Please provide privous password, new password and confirm password of new one."
+    );
   }
 
-  if (req.body.password !== req.body.passwordConfirm) {
+  if (!(await user.correctPassword(oldPassword))) {
+    res.status(401);
+    throw new Error("Old password is incorrect. ");
+  }
+
+  if (req.body.newPassword !== req.body.passwordConfirm) {
     res.status(400);
     throw new Error("Password is not same");
   }
 
-  user.password = req.body.password;
+  user.password = req.body.newPassword;
   await user.save();
 
   user.excludePasswordField();
-  res.send(user);
+  res.status(200).json({
+    status: "success",
+    token: user.generateToken(),
+    user,
+  });
 });
 
 module.exports = {
