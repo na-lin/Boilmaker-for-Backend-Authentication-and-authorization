662dbb13a578fcddd149e499d45ba431a0bc277a
Na Lin <lna483018@gmail.com>
2023-01-01 01:25:52 -0500

    fix bug in update password, check password and password confirm exist in body before compare it

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,15 +4,18 @@ const { User } = require("../db");
 const updatePassword = asyncHandler(async (req, res, next) => {
   const user = await User.findByPk(req.params.id);
 
+  const { password, passwordConfirm } = req.body;
+  if (!password || !passwordConfirm) {
+    res.status(400);
+    throw new Error("Please provide password and password confirm.");
+  }
+
   if (req.body.password !== req.body.passwordConfirm) {
     res.status(400);
     throw new Error("Password is not same");
   }
-  if (req.body.password) {
-    user.password = req.body.password;
-  }
 
-  // save change to db
+  user.password = req.body.password;
   await user.save();
 
   user.excludePasswordField();
