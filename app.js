e07642c49232ed266247368f8cf7c5c59ad2bbba
Na Lin <lna483018@gmail.com>
2022-12-31 20:51:15 -0500

    add morgan and test route to start

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+// @desc Test route
+app.get("/test", (req, res) => {
+  res.send("APP for Learning Auth");
+});
 
 module.exports = app;
