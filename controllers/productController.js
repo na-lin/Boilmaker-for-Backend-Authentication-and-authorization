1b2e33b0008338bee5cc589e2b86d061a7daca12
Na Lin <lna483018@gmail.com>
2022-12-31 21:34:09 -0500

    route: get all products

---
+++ controllers/productController.js
@@ -0,0 +1,14 @@
+const asyncHandler = require("express-async-handler");
+const { Product } = require("../db");
+
+// @desc Get all products
+// @route: GET /api/products/
+// @access: Public
+const getAllProduct = asyncHandler(async (req, res, next) => {
+  const products = await Product.findAll();
+  res.status(200).json(products);
+});
+
+module.exports = {
+  getAllProduct,
+};
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { getAllProduct } = require("../controllers/productController");
 
 const router = express.Router();
+
+router.route("/").get(getAllProduct);
 
 module.exports = router;
