5f0f2b8ac9b490c0401b7b06886c024790c81d6b
Na Lin <lna483018@gmail.com>
2022-12-31 21:33:52 -0500

    use uuid as primary key id

--- db/models/productMode.js
+++ db/models/productMode.js
@@ -2,6 +2,11 @@ const db = require("../database");
 const Sequelize = require("sequelize");
 
 const Product = db.define("product", {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+  },
   name: Sequelize.STRING,
   price: Sequelize.FLOAT,
   brand: Sequelize.STRING,
