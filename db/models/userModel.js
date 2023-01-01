d447e870d15d088b09e83457d292906f60b8f7ad
Na Lin <lna483018@gmail.com>
2022-12-31 22:25:20 -0500

    bcrypt password before save

--- db/models/userModel.js
+++ db/models/userModel.js
@@ -1,5 +1,7 @@
 const db = require("../database");
 const Sequelize = require("sequelize");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = db.define("user", {
   name: {
@@ -29,5 +31,26 @@ const User = db.define("user", {
     },
   },
 });
+
+// @desc: hash password only if password is modified
+User.addHook("beforeSave", async (user) => {
+  // check if modify the password field
+  if (!user.changed("password")) return;
+
+  // hash password
+  user.password = await bcrypt.hash(user.password, 12);
+
+  // clear passwordConfirm field
+  user.passwordConfirm = "";
+});
+
+// @desc: exclude password, passwordConfirm field
+User.prototype.excludePasswordField = function () {
+  this.password = undefined;
+  this.passwordConfirm = undefined;
+  return this;
+};
+
+// @desc: check if passwordConfirm === password
 
 module.exports = User;
