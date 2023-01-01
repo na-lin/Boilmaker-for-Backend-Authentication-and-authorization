893d4806965cf8f9c9329abfaf7cbf61cbd6c93e
Na Lin <lna483018@gmail.com>
2022-12-31 20:47:41 -0500

    connect db to server

---
+++ db/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require("sequelize");
+
+const db = new Sequelize("postgres://localhost:5432/auth", {
+  logging: false, // unless you like the logs
+  // ...and there are many other options you may want to play with
+});
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const dotenv = require("dotenv");
 dotenv.config();
-
+const db = require("./db/database");
 const app = require("./app");
 
+db.sync()
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => {
+    console.log("db connect fail");
+    console.log(err);
+  });
 app.listen(3010, () => {
   console.log("listen to port: 3010");
 });
