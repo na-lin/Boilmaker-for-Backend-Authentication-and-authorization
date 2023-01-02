13154dccf8e26dafbdd0bb19d831a3a6f432ea91
Na Lin <lna483018@gmail.com>
2023-01-01 21:22:35 -0500

    add dev and prod error handler

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,26 +1,64 @@
+const AppError = require("../utils/appError");
 // @desc 404 Not Found error message
 // @route -
 // @access -
 const notFound = (req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
+  const error = new AppError(`Not Found - ${req.originalUrl}`, 404);
   next(error);
+};
+
+const handleValidationErrorDB = (err) => {
+  const originalError = Object.values(err)[1][0].original;
+  originalError.message = Object.values(err)[1][0].message;
+  return originalError;
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // programming or other unknown error
+    console.error("ERROR 💥", err);
+
+    res.status(500).json({
+      status: "error",
+      message:
+        "Something went very wrong! Please wait for a second then try it again.",
+      err,
+    });
+  }
 };
 
 // @desc Global Error handler
 // @route -
 // @access -
 const glbalErrorHandler = (err, req, res, next) => {
-  const statusCode = err.status
-    ? err.status
-    : res.statusCode === 200
-    ? 500
-    : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else {
+    let error = { ...err };
+    error.message = err.message;
+
+    if (err.name === "SequelizeValidationError") {
+      error = handleValidationErrorDB(error);
+    }
+
+    sendErrorProd(error, res);
+  }
 };
 
 module.exports = {
